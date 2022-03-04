457d37caef523d593d2225c5179df84d6674efbc
David Moravec <david.moravec@cfdsupport.com>
2022-03-04 15:16:00 +0100

    hezka ctvercova sit u ostrych hran zhrube 1M bunek

--- examples/nozzle-fda.py
+++ examples/nozzle-fda.py
@@ -7,8 +7,10 @@ sys.path.append("/local/home/moravec/bin/classy_examples")
 from classy_blocks.classes.mesh import Mesh
 from classy_blocks.classes.shapes import Frustum, Cylinder
 
-bl_thickness_def = 0.01
-core_size_def = 0.15
+bl_thickness_def = 0.2
+core_size_def = 0.18
+size_small = 0.2
+size_big = 5  
 
 class nozzleFda:
     def __init__(self):
@@ -23,10 +25,10 @@ class nozzleFda:
         self.z = z
         radius = self.radius
 
-        self.create_cylinder(z[0], z[1], radius[0], 'cylinder1-master', 'inlet')
-        self.create_nozzle('nozzle-master', 'cylinder1-slave')
-        self.create_cylinder(z[2], z[3], radius[1], 'cylinder2-master', 'nozzle-slave', core_size=0.075)
-        self.create_cylinder(z[3], z[4], radius[0], 'outlet', 'cylinder2-slave')
+        self.create_cylinder(z[0], z[1], radius[0], 'cylinder1-master', 'inlet', start_size=size_big, end_size=size_small)
+        self.create_nozzle('nozzle-master', 'cylinder1-slave', core_size=0.12)
+        self.create_cylinder(z[2], z[3], radius[1], 'cylinder2-master', 'nozzle-slave', core_size=0.1, start_size=size_small, end_size=size_small)
+        self.create_cylinder(z[3], z[4], radius[0], 'outlet', 'cylinder2-slave', start_size=size_small, end_size=size_big)
 
         self.mesh.merge_patches('cylinder1-master', 'cylinder1-slave')
         self.mesh.merge_patches('nozzle-master', 'nozzle-slave')
@@ -34,7 +36,7 @@ class nozzleFda:
 
 
 
-    def create_cylinder(self, z1, z2, r, patch_top, patch_bottom, bl_thickness=bl_thickness_def, core_size=core_size_def, count=30):
+    def create_cylinder(self, z1, z2, r, patch_top, patch_bottom, bl_thickness=bl_thickness_def, core_size=core_size_def, start_size=size_small, end_size=size_small):
 
         axis_pt1 = [0, 0, z1]
         axis_pt2 = [0, 0, z2]
@@ -45,13 +47,13 @@ class nozzleFda:
         cylinder.set_bottom_patch(patch_bottom)
         cylinder.set_top_patch(patch_top)
 
-        cylinder.chop_axial(count=30)
+        cylinder.chop_axial(start_size=start_size, end_size=end_size)
         cylinder.chop_radial(start_size=bl_thickness, end_size=core_size)
         cylinder.chop_tangential(start_size=core_size)
 
         self.mesh.add(cylinder)
 
-    def create_nozzle(self, patch_top, patch_bottom, bl_thickness=bl_thickness_def, core_size=core_size_def):
+    def create_nozzle(self, patch_top, patch_bottom, bl_thickness=bl_thickness_def, core_size=core_size_def, start_size=size_small, end_size=size_small):
         z1 = self.z[1]
         z2 = self.z[2]
         r1 = self.radius[0]
@@ -66,7 +68,7 @@ class nozzleFda:
         nozzle.set_bottom_patch(patch_bottom)
         nozzle.set_top_patch(patch_top)
 
-        nozzle.chop_axial(count=30)
+        nozzle.chop_axial(start_size=start_size, end_size=end_size)
         nozzle.chop_radial(start_size=bl_thickness, end_size=core_size)
         nozzle.chop_tangential(start_size=core_size)
 
